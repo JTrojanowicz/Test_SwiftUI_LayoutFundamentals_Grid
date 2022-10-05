a75e8c245c2368658dd7445ff6ab382ffa9cf53a
JTrojanowicz <jaroslaw.trojanowicz@gmail.com>
2022-10-05 09:42:03 +0200

    Initial Commit

---
+++ Test_SwiftUI_LayoutFundamentals_Grid/Test_SwiftUI_LayoutFundamentals_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftUI_LayoutFundamentals_GridApp.swift
+//  Test_SwiftUI_LayoutFundamentals_Grid
+//
+//  Created by Jaroslaw Trojanowicz on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftUI_LayoutFundamentals_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
